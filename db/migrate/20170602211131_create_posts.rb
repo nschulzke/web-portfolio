cd3cd178ae6af4edfafe8beb3fd94592516e1209
Nathan Schulzke <nschulzke@gmail.com>
2017-06-02 15:21:16 -0600

    Rename Blog to Post

--- db/migrate/20170602211131_create_posts.rb
+++ db/migrate/20170602211131_create_posts.rb
@@ -1,6 +1,6 @@
-class CreateBlogs < ActiveRecord::Migration[5.1]
+class CreatePosts < ActiveRecord::Migration[5.1]
   def change
-    create_table :blogs do |t|
+    create_table :posts do |t|
       t.string :title
       t.text :body
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602184852) do
+ActiveRecord::Schema.define(version: 20170602211132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
