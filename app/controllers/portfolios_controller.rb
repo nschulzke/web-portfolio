6359e7adb5405d3a37d384f06de0ea0ded3206eb
Nathan Schulzke <nschulzke@gmail.com>
2017-06-11 01:55:11 +0000

    Add edit and update actions to portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,8 +7,12 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
   
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -18,4 +22,21 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  
+  private
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 end
