fd51b30c924bee726fba2a5003bb3df565be7229
Nathan Schulzke <nschulzke@gmail.com>
2017-06-17 20:26:33 +0000

    Add edit and new buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :portfolios
   get 'pages/home'
-
   get 'pages/about'
-
   get 'pages/contact'
 
   resources :posts
+  resources :portfolios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
