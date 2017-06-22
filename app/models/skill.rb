249302d06e32217144d3979a2a49df5c9399e4da
Nathan Schulzke <nschulzke@gmail.com>
2017-06-22 21:46:40 +0000

    Add concern for placeholders

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,11 @@
 class Skill < ApplicationRecord
+  include Placeholder
+  
   validates_presence_of :title, :percent_focused
+  
+  after_initialize :set_defaults
+  
+  def set_defaults
+    self.thumb_image ||= Placeholder.image_generator( height: '50', width: '50' )
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622211334) do
+ActiveRecord::Schema.define(version: 20170622214123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170622211334) do
     t.integer "percent_focused"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
