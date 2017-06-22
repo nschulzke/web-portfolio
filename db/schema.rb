93ca4d3d9f21327c2134ade644fab6a08544943c
Nathan Schulzke <nschulzke@gmail.com>
2017-06-22 21:14:28 +0000

    Add topic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617211205) do
+ActiveRecord::Schema.define(version: 20170622210950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 20170617211205) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_focused"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
