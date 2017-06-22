4b94903fec5d6057b79142da2e7c9a500fa021d7
Nathan Schulzke <nschulzke@gmail.com>
2017-06-22 21:15:38 +0000

    Fix add topic reference migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622210950) do
+ActiveRecord::Schema.define(version: 20170622211334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,9 @@ ActiveRecord::Schema.define(version: 20170622210950) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
   create_table "skills", force: :cascade do |t|
@@ -60,4 +62,5 @@ ActiveRecord::Schema.define(version: 20170622210950) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "topics"
 end
