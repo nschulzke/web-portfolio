60b5bc4a711433003b70362419528ed6119e61e5
Nathan Schulzke <nschulzke@gmail.com>
2017-06-28 03:13:05 +0000

    Customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :posts do
     member do
       get :toggle_status
