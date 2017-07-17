cd135f128105e564e5579e43dda9604e1a81b041
Nathan Schulzke <nschulzke@gmail.com>
2017-07-17 23:59:17 +0000

    Update styling for main layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper css_class
     if current_user.has_roles?(:guest)
-      (link_to "Register", new_user_registration_path) +
+      (link_to "Register", new_user_registration_path, class: css_class) +
       " " +
-      (link_to "Login", new_user_session_path)
+      (link_to "Login", new_user_session_path, class: css_class)
     else
-      (link_to "Logout", destroy_user_session_path, method: :delete)
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: css_class)
     end
   end
   
