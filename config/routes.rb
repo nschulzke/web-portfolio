c732293aa0cda0e6d90c04bdc10441ffd121d1a5
Nathan Schulzke <nschulzke@gmail.com>
2017-08-03 18:20:36 -0600

    Add tweets page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  
+  get 'tech-news', to: 'pages#tech_news'
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :posts do
     member do
@@ -12,8 +13,8 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
-  
-  
+
+
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'show_portfolio'
 end
