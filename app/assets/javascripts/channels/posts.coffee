1c27a43d75fe69a19a726088bfa795665dd10886
Nathan Schulzke <nschulzke@gmail.com>
2017-08-04 21:27:06 -0600

    Finish actioncable for comments

--- app/assets/javascripts/channels/posts.coffee
+++ app/assets/javascripts/channels/posts.coffee
@@ -7,17 +7,16 @@ jQuery(document).on 'turbolinks:load', ->
     },
     connected: ->
     disconnected: ->
-    received:
+    received: (data) ->
       $comments.append data['comment']
     send_comment: (comment, post_id) ->
       @perform 'send_comment', comment: comment, post_id: post_id
-
   $('#new_comment').submit (e) ->
     $this = $(this)
     textarea = $this.find('#comment_content')
     if $.trim(textarea.val()).length > 1
       App.global_chat.send_comment textarea.val(),
-      comments.data('post-id')
+      $comments.data('post-id')
       textarea.val('')
     e.preventDefault()
     return false
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,26 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def guest_user
+      User.new(
+        name: "Guest User",
+        email: "guest@example.com",
+        roles: "guest"
+      )
+    end
+
+    def connect
+      self.current_user = find_verified_user || guest_user
+      logger.add_tags 'ActionCable', current_user.email
+      logger.add_tags 'ActionCable', current_user.id
+    end
+
+    protected
+      def find_verified_user
+        if verified_user = env['warden'].user
+          verified_user
+        end
+      end
   end
 end
