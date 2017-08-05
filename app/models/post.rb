cb14dcb9ea9d65ba914775dbb7582a9953bf6c39
Nathan Schulzke <nschulzke@gmail.com>
2017-08-04 20:30:48 -0600

    Build out comments resource

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,10 @@ class Post < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
+
   validates_presence_of :title, :body
-  
+
   belongs_to :topic
+
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,22 @@ class User < ApplicationRecord
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
   petergate(roles: [:site_admin, :guest], multiple: false)                                      ##
-  ############################################################################################ 
- 
+  ############################################################################################
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
   validates_presence_of :name
+
+  has_many :comments, dependent: :destroy
   
   def first_name
     self.name.split.first
   end
-  
+
   def last_name
     self.name.split.last
   end
