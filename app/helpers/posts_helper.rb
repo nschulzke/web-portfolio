f20be37d1bfdcfd9e0daa6893f4798b9c40c14b6
Nathan Schulzke <nschulzke@gmail.com>
2017-08-04 21:46:23 -0600

    Style comments section

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def metadata_helper post
     "Published #{distance_of_time_in_words post.created_at, Time.now} ago"
   end
+
+  def gravatar_helper user
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  end
 end
